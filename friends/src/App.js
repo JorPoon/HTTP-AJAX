cad38eee401e05d7d4b33535a4085ba42da324c0
Jor_Poon <jormpoon@gmail.com>
2019-02-27 16:36:29 -0500

    rendered out the res.data

--- friends/src/App.js
+++ friends/src/App.js
@@ -1,12 +1,43 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './App.css';
 
+
 class App extends Component {
+  constructor() {
+    super();
+    this.state= {
+      friendsList: [],
+      error: ''
+    }
+  }
+
+  componentDidMount(){
+    console.log(`CDM`)
+    axios
+    .get(' http://localhost:5000/friends')
+    .then(res => {
+      console.log(res)
+      this.setState({friendsList: res.data});
+    })
+    .catch(err => {
+      console.log(err)
+      this.setState({error: err});
+    })
+  }
+
   render() {
     return (
       <div className="App">
-
+          {this.state.friendsList.map(friend => {
+             console.log(friend);
+             return <div key={friend.id}>
+                      <h1>{friend.name}</h1>
+                      <p>{friend.age}</p>
+                      <p>{friend.email}</p>
+                    </div>
+          })}
       </div>
     );
   }
