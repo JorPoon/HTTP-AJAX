07807f06c969436c0a13db134e48732c76dcf9b7
Jor_Poon <jormpoon@gmail.com>
2019-02-27 16:59:32 -0500

    created newfriends component

--- friends/src/Friends/NewFriends.js
+++ friends/src/Friends/NewFriends.js
@@ -1,1 +1,13 @@
+import React from 'react'
 
+const NewFriends = () => {
+    return (
+        <form>
+        <input type="text" placeholder='name' />
+        <input type="text" placeholder='age'/>
+        <input type="text" placeholder='email'/>
+      </form>
+    )
+}
+
+export default NewFriends;
